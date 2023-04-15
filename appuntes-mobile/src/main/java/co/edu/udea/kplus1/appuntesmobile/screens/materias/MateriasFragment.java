ee1a981b0d116b52bff9ea79ddb5cac40fcd1e41
kellytbn <kellytbn14@gmail.com>
2023-04-14 21:48:44 -0500

    Configuracion inicial del proyecto

---
+++ appuntes-mobile/src/main/java/co/edu/udea/kplus1/appuntesmobile/screens/materias/MateriasFragment.java
@@ -0,0 +1,14 @@
+package co.edu.udea.kplus1.appuntesmobile.screens.materias;
+
+import androidx.fragment.app.Fragment;
+
+import co.edu.udea.kplus1.appuntesmobile.databinding.MateriasFragmentBinding;
+
+public class MateriasFragment extends Fragment {
+
+    private MateriasFragmentBinding binding;
+    private MateriasViewModel viewModel;
+
+    public MateriasFragment() {
+    }
+}
---
+++ appuntes-mobile/src/main/java/co/edu/udea/kplus1/appuntesmobile/screens/materias/MateriasViewModel.java
@@ -0,0 +1,13 @@
+package co.edu.udea.kplus1.appuntesmobile.screens.materias;
+
+import android.util.Log;
+
+import androidx.lifecycle.ViewModel;
+
+public class MateriasViewModel extends ViewModel {
+
+    public MateriasViewModel() {
+
+        Log.i("ViewModel", "MateriasViewModel created!");
+    }
+}
