05dc8db0ec146b3234fe9a66bc78ed394fb16a1a
kellytbn <kellytbn14@gmail.com>
2023-04-14 22:24:41 -0500

    Se crea navigation para materias

--- appuntes-mobile/src/main/java/co/edu/udea/kplus1/appuntesmobile/screens/materias/MateriasFragment.java
+++ appuntes-mobile/src/main/java/co/edu/udea/kplus1/appuntesmobile/screens/materias/MateriasFragment.java
@@ -1,7 +1,17 @@
 package co.edu.udea.kplus1.appuntesmobile.screens.materias;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import co.edu.udea.kplus1.appuntesmobile.R;
 import co.edu.udea.kplus1.appuntesmobile.databinding.MateriasFragmentBinding;
 
 public class MateriasFragment extends Fragment {
@@ -10,5 +20,25 @@ public class MateriasFragment extends Fragment {
     private MateriasViewModel viewModel;
 
     public MateriasFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.materias_fragment, container, false);
+
+        viewModel = new ViewModelProvider(this).get(MateriasViewModel.class);
+
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        setBarTitle();
+
+        return binding.getRoot();
+    }
+
+    private void setBarTitle() {
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setTitle(R.string.app_bar_materias);
+        }
     }
 }
