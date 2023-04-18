67b72411bdcca55e08ac0e49f90ebc20415cb0fa
RamiroMonroy <ramiro.monroy@udea.edu.co>
2023-04-18 14:59:29 -0500

    Crear apuntes

---
+++ appuntes-mobile/src/main/java/co/edu/udea/kplus1/appuntesmobile/model/ApunteTexto.java
@@ -0,0 +1,18 @@
+package co.edu.udea.kplus1.appuntesmobile.model;
+
+public class ApunteTexto {
+
+    public String apunte;
+
+    public ApunteTexto(String apunte) {
+        this.apunte = apunte;
+    }
+
+    public String getApunte() {
+        return apunte;
+    }
+
+    public void setApunte(String apunte) {
+        this.apunte = apunte;
+    }
+}
---
+++ appuntes-mobile/src/main/java/co/edu/udea/kplus1/appuntesmobile/screens/materias/ApunteTextoViewModel.java
@@ -0,0 +1,13 @@
+package co.edu.udea.kplus1.appuntesmobile.screens.materias;
+
+import android.util.Log;
+
+import androidx.lifecycle.ViewModel;
+
+public class ApunteTextoViewModel extends ViewModel {
+
+    public ApunteTextoViewModel() {
+
+        Log.i("ViewModel", "ApunteTexoViewModel created!");
+    }
+}
