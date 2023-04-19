0ceb1c536b78526659c0f8182de39c1b9b85abff
berracode <juanguillermoalarcon@gmail.com>
2023-04-18 22:06:45 -0500

    Se agrega navegación a  apuntes publicos.

---
+++ appuntes-mobile/src/main/java/co/edu/udea/kplus1/appuntesmobile/screens/materias/MateriasPublicasFragment.java
@@ -0,0 +1,66 @@
+package co.edu.udea.kplus1.appuntesmobile.screens.materias;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.edu.udea.kplus1.appuntesmobile.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MateriasPublicasFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MateriasPublicasFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MateriasPublicasFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MateriasPublicasFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MateriasPublicasFragment newInstance(String param1, String param2) {
+        MateriasPublicasFragment fragment = new MateriasPublicasFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.materias_publicas_fragment, container, false);
+    }
+}
